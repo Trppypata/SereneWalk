aaf5b78346789316931fc8551ded0cf62c3cc607
narra <ermanfaminiano020@gmail.com>
2025-05-04 06:24:06 +0800

    new push

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,5 +3,7 @@ const userController = require("../controller/userController.js")
 const router = express.Router();
 
 router.post("/register", userController.Register)
+router.post("/login", userController.Login)
+router.put("/profile-image", userController.UpdateProfileImage)
 
 module.exports = router;
